76e2c0ca2df492f0f2e6c2dbf67f3e0a5000dc5f
伯才 <xiaoqi.huxq@alibaba-inc.com>
2013-08-08 13:22:57 +0800

    add demo

--- util.php
+++ util.php
@@ -5,25 +5,18 @@
 	switch ($method) {
 		case 'getDemoUrls':
 			$chartType = isset($_GET['type']) ? $_GET['type'] : "linechart";
-			// $all_files = scandir("../gallery/kcharts/1.1/demo/".$chartType."/");
-			$fp = fsockopen("gallery.kissyui.com",80);
-			print_r($fp);
-			$file="GET  /xssh/kcharts/1.1/demo/\nHost:gallery.kissyui.com\n\n";  
-		    fputs($fp,$file);  
-		     while(!feof($fp)){
-		     	echo  fgets($fp,100);
-		     }  
-              fclose($fp);  
-			// $files = array_splice($all_files, 2);
-			// $__files = array();
+			$all_files = scandir("../gallery/kcharts/1.1/demo/".$chartType."/");
+			
+			$files = array_splice($all_files, 2);
+			$__files = array();
 
-			// foreach ($files as $key => $value){
-			// 	// 仅读取php html文件
-			// 	if(strstr($value,".html") || strstr($value,".php")){
-			// 		  array_push($__files, $value);
-			// 	}
-			// }
-			// echo json_encode($__files);
+			foreach ($files as $key => $value){
+				// 仅读取php html文件
+				if(strstr($value,".html") || strstr($value,".php")){
+					  array_push($__files, $value);
+				}
+			}
+			echo json_encode($__files);
 			break;
 		
 		default:
