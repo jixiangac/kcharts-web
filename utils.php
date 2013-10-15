05303907b42748bb37ee63ef08b15f349113a6ac
伯才 <xiaoqi.huxq@alibaba-inc.com>
2013-10-15 11:46:43 +0800

    加入转换器

--- utils.php
+++ utils.php
@@ -3,12 +3,30 @@
 
 ?>
 <style>
-
+.ctn{
+	width:1000px;
+	margin:0px auto;
+	padding: 20px 0px;
+	min-height: 400px;
+	background: #fff;
+}
+ul li{
+	color:#ccc;
+	line-height: 26px;
+	border-bottom: 1px dotted #f7f7f7;
+	padding: 2px 20px;
+}
+h1{
+	font-size: 30px;
+	padding: 10px;
+}
 </style>
-<div>
+<div class="ctn">
+	<h1>Util</h1>
 <ul>
 	<li><a href="http://kcharts.taobao.net/dataviz/index.php" target="_blank">在线数据画报生成器</a></li>
 	<li><a href="http://kcharts.taobao.net/dataviz/dataformat.php" target="_blank">在线数据转换工具</a></li>
+	<li>comming soon...</li>
 </ul>
 </div>
 <?php
