4fe86d3b5e33d887e26f2130c55b9e76692dd6ba
伯才 <xiaoqi.huxq@alibaba-inc.com>
2014-01-28 14:33:51 +0800

    备案号

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 			<li class="mapchart">地图</li>
 		</ul>
 		<span class="powerby">
-			Powered by Kissy 数据可视化小组 浙ICP备14003274号 <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_5806286'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/stat.php%3Fid%3D5806286%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+			Powered by Kissy 数据可视化小组 <a target="_blank" href="http://www.miitbeian.gov.cn/">浙ICP备14003274号</a> <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_5806286'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/stat.php%3Fid%3D5806286%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
 		</span>
 	</div>
 </div>
