0bc4c60cd538271a62251946a33ae0f5a8a8a4d6
伯才 <xiaoqi.huxq@alibaba-inc.com>
2014-02-05 19:47:02 +0800

    标题

--- header.php
+++ header.php
@@ -8,9 +8,8 @@
 <meta charset="utf-8"/>
 <meta name="baidu-site-verification" content="kc2vd1sYCO" />
 <meta name="description" content="KCharts,kissy,raphael,基于Kissy的图表,数据可视化,js图表组件,KCharts是基于kissy的数据可视化组件，包含折线图、柱状图、饼图、散点图等图表，定制方便，快速上手。" />
-
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-<title>KCharts</title>
+<title>KCharts-基于Kissy的图表组件集</title>
 <link rel="Shortcut Icon" href="favicon.ico" />
 <link rel="stylesheet" href="http://a.tbcdn.cn/p/global/1.0/global-min.css" />
 <link rel="stylesheet" href="css/index.css" />
@@ -21,6 +20,7 @@
 	$location = $_SERVER['PHP_SELF'];
 ?>
 <body>
+<div class="kc-header-wrapper">
 <div class="kc-header">
 	<div class="inner">
 		<ul class="nav" id="nav">
@@ -28,12 +28,12 @@
 			<li <?php if(strstr($location, "lib.php")){ ?>class="cur" <?php } ?>><a href="lib.php">图表库</a></li>
 			<li style="display:none" <?php if(strstr($location, "demo.php")){ ?>class="cur" <?php } ?>><a href="demo.php">Demo贡献</a></li>
 			<li <?php if(strstr($location, "generator.php")){ ?>class="cur" <?php } ?>><a href="generator.php">生成器</a></li>
-			<li><a target="_blank" href="http://gallery.kissyui.com/kcharts/1.3/guide/index.html?spm=0.0.0.0.WpVybg">文档</a></li>
+			<li><a target="_blank" href="http://gallery.kissyui.com/kcharts/1.3/guide/index.html">文档</a></li>
 			<li style="display:none" <?php if(strstr($location, "utils.php")){ ?>class="cur" <?php } ?>><a href="utils.php">工具</a></li>
 			<li><a href="https://github.com/kissygalleryteam/kcharts" target="_blank">github</a></li>
 			<li class="box"></li>
 		</ul>
 	</div>
-
+</div>
 </div>
 <script type="text/javascript" src="js/nav.js"></script>
